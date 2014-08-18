bf7af64bae40ff3dafd45c28f9c407c297c227a1
hongxun.lhx <hongxun.lhx@alibaba-inc.com>
2014-08-18 11:24:35 +0800

    translate some index file

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,6 +32,42 @@ module.exports = function (grunt) {
             dest: 'get-started/',
             ext: '.html'
           }]
+        },
+        apps: {
+          files: [{
+            expand: true,
+            cwd: 'apps/',
+            src: ['*.jade'],
+            dest: 'apps/',
+            ext: '.html'
+          }]
+        },
+        examples: {
+          files: [{
+            expand: true,
+            cwd: 'examples/',
+            src: ['*.jade'],
+            dest: 'examples/',
+            ext: '.html'
+          }]
+        },
+        showcase: {
+          files: [{
+            expand: true,
+            cwd: 'showcase/',
+            src: ['*.jade'],
+            dest: 'showcase/',
+            ext: '.html'
+          }]
+        },
+        tutorials: {
+          files: [{
+            expand: true,
+            cwd: 'tutorials/',
+            src: ['*.jade'],
+            dest: 'tutorials/',
+            ext: '.html'
+          }]
         }
       },
       watch: {
@@ -46,6 +82,22 @@ module.exports = function (grunt) {
         getStarted: {
           files: ['get-started/*.jade'],
           tasks: ['newer:jade:getStarted']
+        },
+        apps: {
+          files: ['apps/*.jade'],
+          tasks: ['newer:jade:apps']
+        },
+        examples: {
+          files: ['examples/*.jade'],
+          tasks: ['newer:jade:examples']
+        },
+        showcase: {
+          files: ['showcase/*.jade'],
+          tasks: ['newer:jade:showcase']
+        },
+        tutorials: {
+          files: ['tutorials/*.jade'],
+          tasks: ['newer:jade:tutorials']
         }
       }
     });
