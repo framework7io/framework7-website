7c9bd1458053a1665ccd7f08ace728c0abb28184
Vladimir Kharlampidi <nolimits4web@gmail.com>
2017-12-08 11:55:31 +0300

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,321 +1,339 @@
 (function(){
-    'use strict';
-    var gulp = require('gulp'),
-        connect = require('gulp-connect'),
-        open = require('gulp-open'),
-        less = require('gulp-less'),
-        jade = require('gulp-jade'),
-        path = require('path'),
-        fs = require('fs'),
-        del = require('del'),
-        iconsManifest = require('./manifest-icons.json'),
-        useCDN = true,
-        cdnPath = '//cdn.framework7.io',
-        sftp = require('gulp-sftp'),
-        gutil = require( 'gulp-util' ),
-        processVueJadeFiles = require('./src/react-doc-generation/vue-jade-file-processing').processVueJadeFiles,
-        processReactHtmlFiles = require('./src/react-doc-generation/react-html-file-processing').processReactHtmlFiles,
-        paths = {
-            root: './',
-            css: './css',
-            js: './js',
-            src: './src',
-            jade: './src/jade',
-            less: './src/less',
-            'ks-ios': './kitchen-sink-ios',
-            'ks-material': './kitchen-sink-material',
-            plugins: './plugins',
-            examples: './examples',
-            apps: './apps',
-            vue: './vue',
-            react: './react'
-        },
-        pages = {
-            home: {
-                src: './src/jade/index.jade',
-                dest: './'
-            },
-            apps: {
-                src: './src/jade/apps/index.jade',
-                dest: './apps/'
-            },
-            contribute: {
-                src: './src/jade/contribute/index.jade',
-                dest: './contribute/'
-            },
-            icons: {
-                src: './src/jade/icons/index.jade',
-                dest: './icons/'
-            },
-            docs: {
-                src: './src/jade/docs/**/*.jade',
-                dest: './docs/'
-            },
-            'docs-demos': {
-                src: './src/jade/docs-demos/**/*.jade',
-                dest: './docs-demos/'
-            },
-            donate: {
-                src: './src/jade/donate/index.jade',
-                dest: './donate/'
-            },
-            examples: {
-                src: './src/jade/examples/index.jade',
-                dest: './examples/'
-            },
-            'get-started': {
-                src: './src/jade/get-started/index.jade',
-                dest: './get-started/'
-            },
-            plugins: {
-                src: './src/jade/plugins/index.jade',
-                dest: './plugins/'
-            },
-            showcase: {
-                src: './src/jade/showcase/index.jade',
-                dest: './showcase/'
-            },
-            tutorials: {
-                src: './src/jade/tutorials/**/*.jade',
-                dest: './tutorials/'
-            },
-            vue: {
-                src: './src/jade/vue/**/*.jade',
-                dest: './vue/'
-            },
-            react: {
-                src: './react-jade-temp/**/*.jade',
-                dest: './react/'
-            }
-        },
-        pageKeys = [],
-        styles = [
-            {
-                src: './src/less/main.less',
-                dest: './css/'
-            }
-        ];
+  'use strict';
+  var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    open = require('gulp-open'),
+    less = require('gulp-less'),
+    pug = require('gulp-pug'),
+    path = require('path'),
+    fs = require('fs'),
+    del = require('del'),
+    iconsManifest = require('./manifest-icons.json'),
+    useCDN = true,
+    cdnPath = '//cdn.framework7.io',
+    sftp = require('gulp-sftp'),
+    gutil = require( 'gulp-util' ),
+    processVuePugFiles = require('./src/react-doc-generation/vue-pug-file-processing').processVuePugFiles,
+    processReactHtmlFiles = require('./src/react-doc-generation/react-html-file-processing').processReactHtmlFiles,
+    paths = {
+      root: './',
+      css: './css',
+      js: './js',
+      src: './src',
+      pug: './src/pug',
+      less: './src/less',
+      'ks-ios': './kitchen-sink-ios',
+      'ks-material': './kitchen-sink-material',
+      plugins: './plugins',
+      examples: './examples',
+      apps: './apps',
+      vue: './vue',
+      react: './react'
+    },
+    pages = {
+      home: {
+        src: './src/pug/index.pug',
+        dest: './'
+      },
+      apps: {
+        src: './src/pug/apps/index.pug',
+        dest: './apps/'
+      },
+      contribute: {
+        src: './src/pug/contribute/index.pug',
+        dest: './contribute/'
+      },
+      icons: {
+        src: './src/pug/icons/index.pug',
+        dest: './icons/'
+      },
+      docs: {
+        src: './src/pug/docs/**/*.pug',
+        dest: './docs/'
+      },
+      'docs-demos': {
+        src: './src/pug/docs-demos/**/*.pug',
+        dest: './docs-demos/'
+      },
+      donate: {
+        src: './src/pug/donate/index.pug',
+        dest: './donate/'
+      },
+      examples: {
+        src: './src/pug/examples/index.pug',
+        dest: './examples/'
+      },
+      'get-started': {
+        src: './src/pug/get-started/index.pug',
+        dest: './get-started/'
+      },
+      plugins: {
+        src: './src/pug/plugins/index.pug',
+        dest: './plugins/'
+      },
+      showcase: {
+        src: './src/pug/showcase/index.pug',
+        dest: './showcase/'
+      },
+      tutorials: {
+        src: './src/pug/tutorials/**/*.pug',
+        dest: './tutorials/'
+      },
+      vue: {
+        src: './src/pug/vue/**/*.pug',
+        dest: './vue/'
+      },
+      react: {
+        src: './react-pug-temp/**/*.pug',
+        dest: './react/'
+      }
+    },
+    pageKeys = [],
+    styles = [
+      {
+        src: './src/less/main.less',
+        dest: './css/'
+      }
+    ];
 
-    for (var page in pages) {
-        if(pages.hasOwnProperty(page)) pageKeys.push(page);
-    }
+  for (var page in pages) {
+    if(pages.hasOwnProperty(page)) pageKeys.push(page);
+  }
 
-    // Jade Filter
-    require('jade').filters['code'] = function (text) {
-        return text
-        .replace( /</g, '&lt;'   )
-        .replace( />/g, '&gt;'   )
+    // Pug Filter
+    require('pug').filters['code'] = function (text) {
+      return text
+      .replace( /</g, '&lt;'   )
+      .replace( />/g, '&gt;'   )
     }
 
     /* ==================================================================
     Check CDN
     ================================================================== */
     function checkIsLocal(local) {
-        if (local) local = local.toString().replace('-', '');
-        if (local === 'local') {
-            useCDN = false;
-        }
+      if (local) local = local.toString().replace('-', '');
+      if (local === 'local') {
+        useCDN = false;
+      }
     }
     /* ==================================================================
     Build
     ================================================================== */
     // Styles
     gulp.task('less', function (cb) {
-        var cbs = 0;
-        styles.forEach(function (style) {
-            gulp.src([style.src])
-                .pipe(less({
-                    paths: [ path.join(__dirname, 'less', 'includes') ]
-                }))
-                .pipe(gulp.dest(style.dest))
-                .pipe(connect.reload())
-                .on('end', function () {
-                    cbs ++;
-                    if (cbs === styles.length) cb();
-                });
+      var cbs = 0;
+      styles.forEach(function (style) {
+        gulp.src([style.src])
+        .pipe(less({
+          paths: [ path.join(__dirname, 'less', 'includes') ]
+        }))
+        .pipe(gulp.dest(style.dest))
+        .pipe(connect.reload())
+        .on('end', function () {
+          cbs ++;
+          if (cbs === styles.length) cb();
         });
+      });
     });
     // By Sections
-    var pagesJadeTasks = {};
+    var pagesPugTasks = {};
     pageKeys.forEach(function (page) {
-        pagesJadeTasks[page] = function (file) {
-            var src = file ? file : pages[page].src;
-            var fileName = '';
-            if (file) {
-                fileName = file.split('/')[file.split('/').length - 1];
-            }
-            console.log('Starting jade:' + page + ':' + fileName);
-            var time = new Date().getTime();
-            gulp.src(src)
-                .pipe(jade({
-                    pretty: true,
-                    locals: {
-                        cdn: useCDN ? cdnPath : '',
-                        icons: iconsManifest.icons
-                    }
-                }))
-                .pipe(gulp.dest(pages[page].dest))
-                .on('end', function () {
-                    connect.reload();
-                    console.log('Finished jade:' + page + ':' + fileName + ' in ' + (new Date().getTime() - time) + 'ms');
-                });
+      pagesPugTasks[page] = function (file) {
+        var src = file ? file : pages[page].src;
+        var fileName = '';
+        if (file) {
+          fileName = file.split('/')[file.split('/').length - 1];
         }
-        gulp.task('jade-' + page, function (cb) {
-            checkIsLocal(process.argv.slice(3));
-            pagesJadeTasks[page]()
+        console.log('Starting pug:' + page + ':' + fileName);
+        var time = new Date().getTime();
+        gulp.src(src)
+        .pipe(pug({
+          pug: require('pug'),
+          pretty: true,
+          locals: {
+            cdn: useCDN ? cdnPath : '',
+            icons: iconsManifest.icons
+          }
+        }))
+        .pipe(gulp.dest(pages[page].dest))
+        .on('end', function () {
+          connect.reload();
+          console.log('Finished pug:' + page + ':' + fileName + ' in ' + (new Date().getTime() - time) + 'ms');
         });
-    });
-    // All Jade Pages
-    gulp.task('jade', function (cb) {
-        processVueJadeFiles();
+      }
+      gulp.task('pug-' + page, function (cb) {
         checkIsLocal(process.argv.slice(3));
-        var cbs = 0;
-        pageKeys.forEach(function (page) {
-            gulp.src(pages[page].src)
-                .pipe(jade({
-                    pretty: true,
-                    locals: {
-                        cdn: useCDN ? cdnPath : '',
-                        icons: iconsManifest.icons
-                    }
-                }))
-                .pipe(gulp.dest(pages[page].dest))
-                .on('end', function () {
-                    cbs ++;
-                    if (cbs === pageKeys.length) {
-                        connect.reload();
-                        processReactHtmlFiles(cb);
-                    }
-                });
-        });
+        pagesPugTasks[page]()
+      });
+    });
+    // All Pug Pages
+    gulp.task('pug', function (cb) {
+      // processVuePugFiles();
+      checkIsLocal(process.argv.slice(3));
+      var cbs = 0;
+      gulp.src(['./src/pug/**/*.pug', './src/pug/**/!_*.pug'], { cwd: 'src/pug/' })
+        .pipe()
+        .pipe(pug({
+          pug: require('pug'),
+          pretty: true,
+          locals: {
+            cdn: useCDN ? cdnPath : '',
+            icons: iconsManifest.icons
+          }
+        }))
+        .pipe(gulp.dest('./'));
+      // pageKeys.forEach(function (page) {
+      //   gulp.src(pages[page].src)
+      //     .pipe(pug({
+      //       pug: require('pug'),
+      //       pretty: true,
+      //       locals: {
+      //         cdn: useCDN ? cdnPath : '',
+      //         icons: iconsManifest.icons
+      //       }
+      //     }))
+      //     .pipe(gulp.dest(pages[page].dest))
+      //     .on('end', function () {
+      //       cbs ++;
+      //       if (cbs === pageKeys.length) {
+      //         connect.reload();
+      //         // processReactHtmlFiles(cb);
+      //       }
+      //     });
+      // });
     });
 
     gulp.task('process-html', function (cb) {
-        processReactHtmlFiles(cb);
+      processReactHtmlFiles(cb);
     });
 
     // Build All
-    gulp.task('build', ['jade', 'less'], function (cb) {
-        cb();
+    gulp.task('build', ['pug', 'less'], function (cb) {
+      cb();
     });
     gulp.task('build-local', function (cb) {
-        local = true;
+      local = true;
     });
     /* =================================
     Clean Kitchen Sink
     ================================= */
     gulp.task('clean', function (cb) {
-        var toDelete = [
-            paths['ks-ios'] + '/jade',
-            paths['ks-ios'] + '/less',
-            paths['ks-material'] + '/jade',
-            paths['ks-material'] + '/less',
-        ];
-        ['examples', 'apps', 'plugins'].forEach(function (folder) {
-            toDelete.push(paths[folder] + '/**/**/*.jade');
-            toDelete.push(paths[folder] + '/**/**/jade');
-            toDelete.push(paths[folder] + '/**/**/*.less');
-            toDelete.push(paths[folder] + '/**/**/less');
-        });
-        del(toDelete).then(function () {
-            cb();
-        });
+      var toDelete = [
+      paths['ks-ios'] + '/pug',
+      paths['ks-ios'] + '/less',
+      paths['ks-material'] + '/pug',
+      paths['ks-material'] + '/less',
+      ];
+      ['examples', 'apps', 'plugins'].forEach(function (folder) {
+        toDelete.push(paths[folder] + '/**/**/*.pug');
+        toDelete.push(paths[folder] + '/**/**/pug');
+        toDelete.push(paths[folder] + '/**/**/*.less');
+        toDelete.push(paths[folder] + '/**/**/less');
+      });
+      del(toDelete).then(function () {
+        cb();
+      });
     });
     /* =================================
     Watch
     ================================= */
     gulp.task('watch', function () {
-        checkIsLocal(process.argv.slice(3));
+      checkIsLocal(process.argv.slice(3));
 
-        gulp.watch(paths.less + '**/*.*', [ 'less' ]);
-        pageKeys.forEach(function (page) {
-            gulp.watch(pages[page].src, function (data) {
-                if (page === 'docs' || page === 'vue') {
-                    pagesJadeTasks[page](data.path);
-                }
-                else pagesJadeTasks[page]();
-            });
-        });
-        gulp.watch([
-                paths.jade + '/_vars.jade',
-                paths.jade + '/_internal-template.jade',
-                paths.jade + '/_footer.jade',
-                paths.jade + '/_github_buttons.jade',
-                paths.jade + '/_social-buttons.jade'
-            ], ['jade']);
+      gulp.watch(paths.less + '**/*.*', [ 'less' ]);
+      gulp.watch('./src/pug/**/*.pug', (data) => {
+        if (data.type !== 'changed') return;
+        const filePath = data.path.split('/src/pug/')[1];
+        console.log(filePath);
+      });
+      // pageKeys.forEach(function (page) {
+      //   gulp.watch(pages[page].src, function (data) {
+      //     if (page === 'docs' || page === 'vue') {
+      //       pagesPugTasks[page](data.path);
+      //     }
+      //     else pagesPugTasks[page]();
+      //   });
+      // });
+      // gulp.watch([
+      //   paths.pug + '/_vars.pug',
+      //   paths.pug + '/_internal-template.pug',
+      //   paths.pug + '/_footer.pug',
+      //   paths.pug + '/_github_buttons.pug',
+      //   paths.pug + '/_social-buttons.pug'
+      //   ], ['pug']);
     });
     /* =================================
     Deploy
     ================================= */
     gulp.task('deploy', function () {
-        var folder;
-        if (process.argv.slice(3)) {
-            folder = process.argv.slice(3);
-            if (folder) folder = folder.toString().replace('-', '');
-        }
-        var src = [
-            './*.html',
-            './robots.txt',
-            './*.png',
-            './framework7.json',
-            './manifest-icons.json',
-            './apps/**/*.*',
-            './contribute/**/*.*',
-            './css/**/*.*',
-            './dist/**/*.*',
-            './docs/**/*.*',
-            './docs-demos/**/*.*',
-            './donate/**/*.*',
-            './examples/**/*.*',
-            './fonts/**/*.*',
-            './forum/**/*.*',
-            './get-started/**/*.*',
-            './i/**/*.*',
-            './icons/**/*.*',
-            './js/**/*.*',
-            './kitchen-sink-ios/**/*.*',
-            './kitchen-sink-material/**/*.*',
-            './plugins/**/*.*',
-            './showcase/**/*.*',
-            './tutorials/**/*.*',
-            './vue/**/*.*',
-            './react/**/*.*'
-            ];
-        var folderSrc = {
-            'index': './index.html',
-            'apps': './apps/**/*.*',
-            'dist': './dist/**/*.*',
-            'docs': './docs/**/*.*',
-            'docs-demos': './docs/**/*.*',
-            'examples': './examples/**/*.*',
-            'plugins': './plugins/**/*.*',
-            'showcase': './showcase/**/*.*',
-            'tutorials': './tutorials/**/*.*',
-            'vue': './vue/**/*.*',
-            'react': './react/**/*.*',
-            'kitchen-sink': ['./kitchen-sink-ios/**/*.*', './kitchen-sink-material/**/*.*']
-        };
-        if (folder) src = folderSrc[folder];
+      var folder;
+      if (process.argv.slice(3)) {
+        folder = process.argv.slice(3);
+        if (folder) folder = folder.toString().replace('-', '');
+      }
+      var src = [
+      './*.html',
+      './robots.txt',
+      './*.png',
+      './framework7.json',
+      './manifest-icons.json',
+      './apps/**/*.*',
+      './contribute/**/*.*',
+      './css/**/*.*',
+      './dist/**/*.*',
+      './docs/**/*.*',
+      './docs-demos/**/*.*',
+      './donate/**/*.*',
+      './examples/**/*.*',
+      './fonts/**/*.*',
+      './forum/**/*.*',
+      './get-started/**/*.*',
+      './i/**/*.*',
+      './icons/**/*.*',
+      './js/**/*.*',
+      './kitchen-sink-ios/**/*.*',
+      './kitchen-sink-material/**/*.*',
+      './plugins/**/*.*',
+      './showcase/**/*.*',
+      './tutorials/**/*.*',
+      './vue/**/*.*',
+      './react/**/*.*'
+      ];
+      var folderSrc = {
+        'index': './index.html',
+        'apps': './apps/**/*.*',
+        'dist': './dist/**/*.*',
+        'docs': './docs/**/*.*',
+        'docs-demos': './docs/**/*.*',
+        'examples': './examples/**/*.*',
+        'plugins': './plugins/**/*.*',
+        'showcase': './showcase/**/*.*',
+        'tutorials': './tutorials/**/*.*',
+        'vue': './vue/**/*.*',
+        'react': './react/**/*.*',
+        'kitchen-sink': ['./kitchen-sink-ios/**/*.*', './kitchen-sink-material/**/*.*']
+      };
+      if (folder) src = folderSrc[folder];
 
-        var remote = require('./remote.json');
+      var remote = require('./remote.json');
 
-        gulp.src(src, {base: './'})
-            .pipe(sftp(remote));
+      gulp.src(src, {base: './'})
+      .pipe(sftp(remote));
     });
 
     /* =================================
     Server
     ================================= */
     gulp.task('connect', function () {
-        return connect.server({
-            root: [ paths.root ],
-            livereload: true,
-            port:'3000'
-        });
+      return connect.server({
+        root: [ paths.root ],
+        livereload: true,
+        port:'3000'
+      });
     });
 
     gulp.task('open', function () {
-        return gulp.src('./index.html').pipe(open({ uri: 'http://localhost:3000/index.html'}));
+      return gulp.src('./index.html').pipe(open({ uri: 'http://localhost:3000/index.html'}));
     });
 
     gulp.task('server', [ 'watch', 'connect', 'open' ]);
@@ -323,4 +341,4 @@
     gulp.task('default', [ 'server' ]);
 
     gulp.task('test', [ 'build' ]);
-})();
+  })();
