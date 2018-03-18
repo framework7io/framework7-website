31c8771306f30b4237c1aa77a5bef0c7aa373342
Vladimir Kharlampidi <nolimits4web@gmail.com>
2018-03-18 15:02:44 +0300

    2.1.1

--- kitchen-sink/js/routes.js
+++ kitchen-sink/js/routes.js
@@ -243,6 +243,10 @@ var routes = [
     componentUrl: './pages/statusbar.html',
   },
   {
+    path: '/stepper/',
+    componentUrl: './pages/stepper.html',
+  },
+  {
     path: '/subnavbar/',
     url: './pages/subnavbar.html',
   },
@@ -455,10 +459,22 @@ var routes = [
   {
     path: '/page-loader-template7/:user/:userId/:posts/:postId/',
     templateUrl: './pages/page-loader-template7.html',
+    // additional context
+    options: {
+      context: {
+        foo: 'bar',
+      },
+    },
   },
   {
     path: '/page-loader-component/:user/:userId/:posts/:postId/',
     componentUrl: './pages/page-loader-component.html',
+    // additional context
+    options: {
+      context: {
+        foo: 'bar',
+      },
+    },
   },
 
   // Default route (404 page). MUST BE THE LAST
