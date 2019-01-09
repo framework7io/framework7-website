24d57223fa4812fea9863d56de72dbd3ca4ecff8
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-01-09 19:52:42 +0300

    Fixes

--- gulpfile.js
+++ gulpfile.js
@@ -101,9 +101,7 @@
 
 
   // Build All
-  gulp.task('build', ['pug', 'less'], function (cb) {
-    cb();
-  });
+  gulp.task('build', gulp.series('pug', 'less'));
 
   /* =================================
   Watch
@@ -111,11 +109,10 @@
   gulp.task('watch', function () {
     checkIsLocal(process.argv.slice(3));
 
-    gulp.watch('./src/less/**/*.*', [ 'less' ]);
-    gulp.watch('./src/pug/**/*.pug', (data) => {
+    gulp.watch('./src/less/**/*.*', gulp.series('less'));
+    gulp.watch('./src/pug/**/*.pug', { events: ['change'] }).on('change', (changedPath) => {
       checkIsLocal(process.argv.slice(3));
-      if (data.type !== 'changed') return;
-      const filePath = data.path.split('/src/pug/')[1];
+      const filePath = changedPath.split('src/pug/')[1];
       // if (filePath.indexOf('react') === 0) return;
       if (filePath.indexOf('_') === 0 || filePath.indexOf('_layout.pug') >= 0) {
         buildPages();
@@ -169,9 +166,9 @@
     return gulp.src('./index.html').pipe(open({ uri: 'http://localhost:3000/index.html'}));
   });
 
-  gulp.task('server', [ 'watch', 'connect', 'open' ]);
+  gulp.task('server', gulp.parallel('watch', 'connect', 'open' ));
 
-  gulp.task('default', [ 'server' ]);
+  gulp.task('default', gulp.series('server'));
 
-  gulp.task('test', [ 'build' ]);
-  })();
+  gulp.task('test', gulp.series('build'));
+})();
