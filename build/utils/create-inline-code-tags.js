03f4c7750d49aadb9d501c17bcd90677543d2a3a
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-09-20 19:25:23 +0300

    More v5 stuff

--- build/utils/create-inline-code-tags.js
+++ build/utils/create-inline-code-tags.js
@@ -1,6 +1,9 @@
 module.exports = (content = '') => {
   content = content.replace(/`([^\n`]*)`/g, (string, code) => {
     if (string.indexOf('${') >= 0) return string;
+    code = code
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;');
     return `<code>${code}</code>`;
   });
 
