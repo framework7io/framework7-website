5600d21a27d73e3b0da17a27cb321def1cb04d60
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-09-25 17:49:18 +0300

    v5: More v5 docs

--- build/utils/components-with-css-vars.js
+++ build/utils/components-with-css-vars.js
@@ -7,8 +7,9 @@ const withCssVars = [];
 
 modules.forEach((module) => {
   if (module[0] === '.') return;
-  if (fs.existsSync(path.resolve(__dirname, `../../packages/core/components/${module}/${module}-vars.less`))) {
-    const content = fs.readFileSync(path.resolve(__dirname, `../../packages/core/components/${module}/${module}-vars.less`), 'utf8');
+  const cssVarsPath = path.resolve(__dirname, `../../packages/core/components/${module}/${module}-vars.less`);
+  if (fs.existsSync(cssVarsPath)) {
+    const content = fs.readFileSync(cssVarsPath, 'utf8');
     if (content && content.trim().length) withCssVars.push(module);
   }
 });
