b9d6b0384d0c92571e04c75509d11714b6058a8b
Vladimir Kharlampidi <nolimits4web@gmail.com>
2019-09-26 15:34:16 +0300

    v5: Fix tag brackets in code helpers

--- build/utils/code-filter.js
+++ build/utils/code-filter.js
@@ -3,7 +3,9 @@ const highlight = require('./highlight');
 module.exports = (code, { lang } = {}) => {
   code = code
     .replace(/&lt;/g, '<')
-    .replace(/&gt;/g, '>');
+    .replace(/&gt;/g, '>')
+    .replace(/&amp;lt;/g, '&lt;')
+    .replace(/&amp;gt;/g, '&gt;');
 
   code = `<pre><code class="${lang || ''}">${highlight(code, lang)}</code></pre>`;
   return code;
