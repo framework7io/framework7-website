b3acea3f752513c166186535f5d277087023e5c7
Hervé Yvis <mail@yvisherve.net>
2019-10-31 20:13:46 +0100

    Replace "Messsages" by "Messages"

--- kitchen-sink/react/js/app.js
+++ kitchen-sink/react/js/app.js
@@ -82358,7 +82358,7 @@
 	      var _this2 = this;
 
 	      return react.createElement(Page, null, react.createElement(Navbar$2, {
-	        title: "Messsages",
+	        title: "Messages",
 	        backLink: "Back"
 	      }), react.createElement(Messagebar$2, {
 	        placeholder: this.placeholder,
