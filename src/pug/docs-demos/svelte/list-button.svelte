1f30bcf28c803aeb345ea78e6f1101d6d9518e71
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-01-17 21:04:42 +0300

    More svelte demos

---
+++ src/pug/docs-demos/svelte/list-button.svelte
@@ -0,0 +1,28 @@
+<App>
+  <View main>
+    <Page>
+      <Navbar title="List Button" />
+      <List>
+        <ListButton title="List Button 1" />
+        <ListButton title="List Button 2" />
+        <ListButton title="List Button 3" />
+      </List>
+
+      <List inset>
+        <ListButton title="List Button 1" />
+        <ListButton title="List Button 2" />
+        <ListButton title="List Button 3" />
+      </List>
+
+      <List inset>
+        <ListButton title="Red List Button" color="red" />
+        <ListButton title="Green List Button" color="green" />
+        <ListButton title="Orange List Button" color="orange" />
+      </List>
+    </Page>
+  </View>
+</App>
+
+<script>
+  import {App, View, Page, Navbar, List, ListButton} from 'framework7-svelte';
+</script>
---
+++ src/pug/docs-demos/svelte/panel.svelte
@@ -0,0 +1,40 @@
+<App>
+  <Panel resizable left reveal>
+    <View>
+      <Page>
+        <Block>Left panel content</Block>
+      </Page>
+    </View>
+  </Panel>
+  <Panel resizable right themeDark>
+    <View>
+      <Page>
+        <Block>Right panel content</Block>
+      </Page>
+    </View>
+  </Panel>
+  <View main>
+    <Page>
+      <Navbar title="Panel"></Navbar>
+
+      <Block class="row">
+        <Col>
+          <Button raised panelOpen="left">Open left panel</Button>
+        </Col>
+        <Col>
+          <Button raised panelOpen="right">Open right panel</Button>
+        </Col>
+      </Block>
+    </Page>
+  </View>
+</App>
+<style>
+  /* Limit resizable panel width */
+  .panel {
+    min-width: 100px;
+    max-width: 90vw;
+  }
+</style>
+<script>
+  import {App, Panel, View, Page, Block, Navbar, Col, Button} from 'framework7-svelte';
+</script>
