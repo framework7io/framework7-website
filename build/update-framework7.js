2c18afa673ff6356fef08616e7529009bb4ebf85
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-12-29 14:14:26 +0300

    Script to update F7

--- build/update-framework7.js
+++ build/update-framework7.js
@@ -1,4 +1,5 @@
 const { promise: exec } = require('exec-sh');
+const fs = require('fs');
 
 const update = async () => {
   await exec('npm update framework7');
@@ -7,6 +8,21 @@ const update = async () => {
   await exec('npm update framework7-svelte');
   await exec('cp -r ../framework7/packages/* public/packages/');
   await exec('cp -r ../framework7/kitchen-sink/* public/kitchen-sink/');
+
+  const corePkg = require('../public/packages/core/package.json');
+  const currentPkg = require('../package.json');
+
+  const date = new Date();
+  const formattedDate = Intl.DateTimeFormat('en', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  }).format(date);
+
+  currentPkg.releaseVersion = corePkg.version;
+  currentPkg.releaseDate = formattedDate;
+
+  fs.writeFileSync('./package.json', JSON.stringify(currentPkg, '', 2));
 };
 
 update();
