417b9b48587085aec4c4613743621fb432f03b76
Vladimir Kharlampidi <nolimits4web@gmail.com>
2020-12-31 17:06:55 +0300

    git track demos

---
+++ public/docs-demos/core/cards.js
@@ -0,0 +1,18 @@
+let theme = 'ios';
+if (window.location.href.indexOf('theme=md') >= 0) theme = 'md';
+if (window.location.href.indexOf('theme=aurora') >= 0) theme = 'aurora';
+
+const themePlugin = {
+  params: {
+    theme,
+  },
+};
+
+// eslint-disable-next-line
+Framework7.use(themePlugin);
+
+// eslint-disable-next-line
+const app = new Framework7({
+  el: '#app',
+  componentUrl: './cards.f7.html',
+});
