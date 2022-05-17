5a0b404f09e5aa950111231f83640aa02ded8995
Vladimir Kharlampidi <nolimits4web@gmail.com>
2022-05-17 11:46:10 +0300

    fix code filter
    fixes #569

--- build/utils/code-inline-filter.js
+++ build/utils/code-inline-filter.js
@@ -1,7 +1,5 @@
 module.exports = (code, { lang } = {}) => {
-  code = code
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;');
+  code = code.replace(/</g, '&lt;').replace(/>/g, '&gt;');
   code = `<code class="${lang || ''}">${code}</code>`;
   return code;
 };
--- build/utils/create-inline-code-tags.js
+++ build/utils/create-inline-code-tags.js
@@ -1,9 +1,8 @@
 module.exports = (content = '') => {
   content = content.replace(/`([^\n`]*)`/g, (string, code) => {
     if (string.indexOf('${') >= 0) return string;
-    code = code
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;');
+    if (string.indexOf('` : $h`') >= 0) return string;
+    code = code.replace(/</g, '&lt;').replace(/>/g, '&gt;');
     return `<code>${code}</code>`;
   });
 
