549e753573c419ce7fed39a77246006ea1ff8e04
Vladimir Kharlampidi <nolimits4web@gmail.com>
2023-03-10 16:08:44 +0300

    Update examplePreview.js

--- examplePreview.js
+++ examplePreview.js
@@ -1,32 +1,26 @@
 const fs = require('fs');
 
-fs.readdirSync('./src/pug/vue').forEach((f) => {
+fs.readdirSync('./src/pug/docs').forEach((f) => {
   if (!f.includes('.pug')) return;
-  let sourceCount = 0;
-  if (!fs.existsSync(`./src/pug/react/${f}`)) return;
-  const reactContentSources = fs
-    .readFileSync(`./src/pug/react/${f}`, 'utf-8')
-    .split('\n')
-    .filter((line) => line.includes('reactSourceFilterNew'))
-    .map((line) => line.trim());
-  let content = fs.readFileSync(`./src/pug/vue/${f}`, 'utf-8');
-  if (!content.includes('vueSource')) return;
+
+  let content = fs.readFileSync(`./src/pug/docs/${f}`, 'utf-8');
+  if (!content.includes('coreSource')) return;
   content = content
     .split('\n')
     .map((line) => {
-      if (line.includes('vueSource')) {
+      if (line.includes('coreSource')) {
         if (line.includes('store.js')) return line;
-        sourceCount += 1;
         const spaces = Array.from({ length: line.length - line.trim().length })
           .map(() => ' ')
           .join('');
-        return (
-          spaces +
-          reactContentSources[sourceCount - 1].replace(`'react'`, 'vue').replace(`.jsx`, '.vue')
-        );
+        const fName = line
+          .split('include:coreSource ../docs-demos/core/')[1]
+          .split('.f7.html')[0]
+          .replace(/_/g, '-');
+        return `${spaces}+reactSourceFilterNew('core', '${fName}.html', '${fName}')`;
       }
       return line;
     })
     .join('\n');
-  fs.writeFileSync(`./src/pug/vue/${f}`, content);
+  fs.writeFileSync(`./src/pug/docs/${f}`, content);
 });
