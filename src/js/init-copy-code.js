153d380e82062d202bec5c34d7c977e7585756b3
Vladimir Kharlampidi <nolimits4web@gmail.com>
2023-08-23 16:07:08 +0300

    chore: copy tweaks

--- src/js/init-copy-code.js
+++ src/js/init-copy-code.js
@@ -4,23 +4,17 @@ import copyToClipboard from './copy-to-clipboard';
 export default function initCopyCode() {
   $('pre').each((preElement) => {
     const $button = $(
-      `<div class="f7-copy-btn">
+      `<button class="f7-copy-btn">
         <i class="f7-icons f7-copy-icon">square_on_square</i>
         <i class="f7-icons f7-check-icon hidden">checkmark_alt</i>
-      </div>`,
+      </button>`,
     );
     $(preElement).append($button);
     $button.on('click', function onClick() {
       const el = this;
       const text = $(el).prev().text();
       console.log(text);
-      copyToClipboard(text, () => {
-        const $toastEl = $(`<div class="f7-demo-icons-toast">Copied to clipboard!</div>`);
-        $toastEl.once('animationend', () => {
-          $toastEl.remove();
-        });
-        $(document.body).append($toastEl);
-      });
+      copyToClipboard(text);
       $button.find('.f7-copy-icon, .f7-check-icon').toggleClass('hidden');
       setTimeout(() => {
         $button.find('.f7-copy-icon, .f7-check-icon').toggleClass('hidden');
